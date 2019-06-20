be727b6c813fd0f830fc320b87ca0f00335afd7e
Peter Fabian <pfabian@vonfvfxf0avhv2g.lan>
2019-06-19 18:52:04 -0700

    add time method

---
+++ time_check.rb
@@ -0,0 +1,16 @@
+
+
+class TimeCheck
+
+	def self.get_minutes
+		require 'time'
+		t1 = Time.now
+		t2 = Time.new(2019, 06, 25, 7, 20, 00)
+		seconds_until = t2 - t1
+		minutes_until = seconds_until/60
+
+		return minutes_until.round(0)
+	end
+
+
+end
