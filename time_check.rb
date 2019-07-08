33e83cf406caf4c74cc69e6d4f4d6076d1e4d1b3
Peter Fabian <pfabian@vonfvfxf0avhv2g.lan>
2019-07-08 08:35:22 -0700

    update time

--- time_check.rb
+++ time_check.rb
@@ -5,7 +5,7 @@ class TimeCheck
 	def self.get_minutes
 		require 'time'
 		t1 = Time.now
-		t2 = Time.new(2019, 06, 25, 14, 20, 00)
+		t2 = Time.new(2019, 08, 04, 14, 05, 00)
 		seconds_until = t2 - t1
 		minutes_until = seconds_until/60
 
